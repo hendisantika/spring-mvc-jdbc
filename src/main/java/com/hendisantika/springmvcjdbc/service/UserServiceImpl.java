4df97ce724146ad5ee1f2554f3bd196dfc7dc3af
Hendi Santika <hendisantika@gmail.com>
2020-01-01 17:47:36 +0700

    #14 - Add UserServiceImpl

---
+++ src/main/java/com/hendisantika/springmvcjdbc/service/UserServiceImpl.java
@@ -0,0 +1,19 @@
+package com.hendisantika.springmvcjdbc.service;
+
+import com.hendisantika.springmvcjdbc.dao.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-mvc-jdbc
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 01/01/20
+ * Time: 17.47
+ */
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserDao userDetailDao;
+}
